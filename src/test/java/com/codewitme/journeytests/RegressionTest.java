42edcf2afa51867f6288c20eaa7f10ccf6781f74
Coder Tester <codewithme@outlook.com>
2016-05-08 11:12:46 +0530

    Added  multiple link test

--- src/test/java/com/codewitme/journeytests/RegressionTest.java
+++ src/test/java/com/codewitme/journeytests/RegressionTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -31,17 +32,22 @@ public class RegressionTest extends Hooks{
 		Assert.assertEquals(actualPassengerList, expectedPassengerList);
 		
 	}
-	@Test(dataProvider="custLogin")
+	@Test(dataProvider="custLogin",enabled=false)
 	public void VerifySuccessfullMultipleLogin(String userName,String password) throws IOException{	
 		String[]expectedPassengerList = {"1","2","3","4"};
 		System.out.println("VerifySuccessfullLogin");
 		loginPage = new LoginPage(driver);
 		loginPage.setUsername(userName);
-		loginPage.setPassword(userName);
+		loginPage.setPassword(password);
 		loginPage.clickSignIn();
 		welcomePage=new WelcomePage(driver);
 		String[] actualPassengerList=welcomePage.getPassengerList();
 		Assert.assertEquals(actualPassengerList, expectedPassengerList);
+		
+	}
+	@Test(dataProvider="allLinks",enabled=true)
+	public void VerifySuccessfullLinkClick(String text) throws IOException{	
+		driver.findElement(By.linkText(text)).click();
 		
 	}
 	@DataProvider(name="custLogin")
@@ -49,5 +55,18 @@ public class RegressionTest extends Hooks{
 		ExcelReader readr=new ExcelReader();
 		Object[][] arrayObject = readr.getData("LoginTest");
 		return arrayObject;
+	}
+	
+	@DataProvider(name="allLinks")
+	public Object[][] allLinks1() throws IOException {
+		BeforeTest();
+		List<WebElement> list=driver.findElements(By.tagName("a"));
+		int rows=list.size();
+		String[][] linkArray=new String[rows][1];
+		for(int i=0;i<rows;i++){
+			linkArray[i][0]=list.get(i).getText();
+		}
+		AfterTest();
+		return linkArray;
 	}
 }
